04ebe2154e328769c503aa62daaa1cb2b5baebdf
Philip Linden <lindenphilipj@gmail.com>
2024-11-03 16:36:06 -0500

    docs: sorta show drag

--- docs/animations/main.py
+++ docs/animations/main.py
@@ -24,16 +24,33 @@ from manim import (
     GrowFromCenter,
     VGroup,
     ValueTracker,
+    PI,
+    TAU,
+    VMobject,
+    MoveAlongPath,
+    there_and_back,
+    rush_into,
+    rush_from,
+    linear,
+    smooth,
+    wiggle,
 )
 
 
 class Balloon(Circle):
+
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs, radius=1, color=RED, fill_opacity=0.8)
+        super().__init__(*args,
+                         **kwargs,
+                         radius=1,
+                         color=RED,
+                         fill_opacity=0.8)
         self.label = Text("Balloon", color=self.color).next_to(self, RIGHT)
         self.label.add_updater(lambda x: x.next_to(self, RIGHT))
 
+
 class Tether(Line):
+
     def __init__(self, *args, **kwargs):
         super().__init__(
             *args,
@@ -45,6 +62,7 @@ class Tether(Line):
 
 
 class PayloadBox(Square):
+
     def __init__(self, *args, position=np.array([0, 0, 0]), **kwargs):
         super().__init__(
             *args,
@@ -79,24 +97,18 @@ class BalloonAssembly:
         return self.balloon, self.payload_box, self.tether
 
     def fade_in_labels(self):
-        return (
-            FadeIn(obj.label)
-            for obj in [
-                self.balloon,
-                self.tether,
-                self.payload_box,
-            ]
-        )
+        return (FadeIn(obj.label) for obj in [
+            self.balloon,
+            self.tether,
+            self.payload_box,
+        ])
 
     def fade_out_labels(self):
-        return (
-            FadeOut(obj.label)
-            for obj in [
-                self.balloon,
-                self.tether,
-                self.payload_box,
-            ]
-        )
+        return (FadeOut(obj.label) for obj in [
+            self.balloon,
+            self.tether,
+            self.payload_box,
+        ])
 
     def collapse_to_dot(self):
         return (
@@ -107,7 +119,8 @@ class BalloonAssembly:
         )
 
     def expand_from_dot(self):
-        self.balloon = Balloon() # for some reason the balloon needs to be recreated
+        self.balloon = Balloon(
+        )  # for some reason the balloon needs to be recreated
         return (
             GrowFromCenter(self.balloon),
             GrowFromPoint(self.payload_box, self.origin),
@@ -117,11 +130,12 @@ class BalloonAssembly:
 
 
 class ControlVolume(Scene):
+
     def construct(self):
         axes = Axes()
         axis_labels = axes.get_axis_labels(
-            Text("x").scale(0.7), Text("y").scale(0.7)
-        )
+            Text("x").scale(0.7),
+            Text("y").scale(0.7))
 
         # Add the balloon assembly to the scene
         balloon_assembly = BalloonAssembly()
@@ -140,13 +154,15 @@ class ControlVolume(Scene):
             stroke_opacity=1,
             fill_opacity=0,
         )
-        control_volume_box.move_to(np.array([0, -1, 0]))  # Move the square down by 1.5
+        control_volume_box.move_to(np.array([0, -1, 0
+                                             ]))  # Move the square down by 1.5
         control_volume_label = Text("Control Volume", color=BLUE).next_to(
-            control_volume_box, UP
-        )
-        control_volume_label.add_updater(lambda x: x.next_to(control_volume_box, UP))
+            control_volume_box, UP)
+        control_volume_label.add_updater(
+            lambda x: x.next_to(control_volume_box, UP))
 
-        self.play(FadeIn(axes), FadeIn(axis_labels), FadeIn(control_volume_box), FadeIn(control_volume_label))
+        self.play(FadeIn(axes), FadeIn(axis_labels),
+                  FadeIn(control_volume_box), FadeIn(control_volume_label))
         self.wait(3)
 
         self.play(*balloon_assembly.collapse_to_dot())
@@ -155,11 +171,13 @@ class ControlVolume(Scene):
         self.wait(1)
 
         # perfect loop
-        self.play(FadeOut(axes), FadeOut(axis_labels), FadeOut(control_volume_box), FadeOut(control_volume_label))
+        self.play(FadeOut(axes), FadeOut(axis_labels),
+                  FadeOut(control_volume_box), FadeOut(control_volume_label))
         self.play(*balloon_assembly.expand_from_dot())
 
 
 class ForceBalance(Scene):
+
     def construct(self):
         # Create the balloon assembly
         balloon_assembly = BalloonAssembly()
@@ -170,42 +188,31 @@ class ForceBalance(Scene):
         self.play(*balloon_assembly.fade_in_labels())
         self.wait(1)
 
-        self.play(
-            *balloon_assembly.fade_out_labels(),
-            *balloon_assembly.collapse_to_dot(),
-            dot.animate.set_fill(WHITE)
-        )
+        self.play(*balloon_assembly.fade_out_labels(),
+                  *balloon_assembly.collapse_to_dot(),
+                  dot.animate.set_fill(WHITE))
 
-        buoyancy_arrow = Arrow(
-            dot.get_center(), dot.get_center() + UP * 2, color=GREEN
-        )
-        buoyancy_label = Text("Buoyancy", color=GREEN).next_to(
-            buoyancy_arrow, UP
-        )
+        buoyancy_arrow = Arrow(dot.get_center(),
+                               dot.get_center() + UP * 2,
+                               color=GREEN)
+        buoyancy_label = Text("Buoyancy",
+                              color=GREEN).next_to(buoyancy_arrow, UP)
         buoyancy_label.add_updater(lambda x: x.next_to(buoyancy_arrow, UP))
         self.play(GrowArrow(buoyancy_arrow), FadeIn(buoyancy_label))
 
-        gravity_arrow = Arrow(
-            dot.get_center(), dot.get_center() + DOWN * 2, color=RED
-        )
+        gravity_arrow = Arrow(dot.get_center(),
+                              dot.get_center() + DOWN * 2,
+                              color=RED)
         gravity_label = Text("Weight", color=RED).next_to(gravity_arrow, DOWN)
         gravity_label.add_updater(lambda x: x.next_to(gravity_arrow, DOWN))
         self.play(GrowArrow(gravity_arrow), FadeIn(gravity_label))
 
-        drag_arrow = Arrow(
-            dot.get_center(), dot.get_center() + DOWN * 1.6 * np.sin(2 * np.pi * 0.5 * self.renderer.time), color=BLUE
-        )
+        drag_arrow = Arrow(dot.get_center(), dot.get_center(), color=BLUE)
         drag_label = Text("Drag", color=BLUE).next_to(drag_arrow, RIGHT)
         drag_label.add_updater(lambda x: x.next_to(drag_arrow, RIGHT))
 
-        group = VGroup(dot, buoyancy_arrow, buoyancy_label, gravity_arrow, gravity_label, drag_arrow, drag_label)
-        
-        t = ValueTracker(0)
-        def wiggle(x):
-            return 2 * np.sin(3 * x) + np.sin(5 * x)
-
-        group.add_updater(lambda x: x.move_to((t.get_value(), wiggle(t.get_value()) * UP))
-        self.play(t.animate.set_value(1))
+        group = VGroup(dot, buoyancy_arrow, buoyancy_label, gravity_arrow,
+                       drag_arrow, drag_label)
         self.wait(2)
 
         # perfect loop
