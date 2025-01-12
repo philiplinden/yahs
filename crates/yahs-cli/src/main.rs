143d753331065b304519479810571065d71eaee1
Philip Linden <lindenphilipj@gmail.com>
2025-01-12 16:01:12 -0500

    ui: basic cli

--- crates/yahs-cli/src/main.rs
+++ crates/yahs-cli/src/main.rs
@@ -1,36 +1,22 @@
 use bevy::{
-    app::{AppExit, ScheduleRunnerPlugin},
     prelude::*,
+    window::{WindowPlugin, ExitCondition},
 };
-use bevy_ratatui::{
-    error::exit_on_error, event::KeyEvent, terminal::RatatuiContext, RatatuiPlugins,
-};
+use bevy_ratatui::RatatuiPlugins;
+use yahs::prelude::SimulatorPlugins;
+use yahs_cli::TuiPlugin;
+
 fn main() {
-    let wait_duration = std::time::Duration::from_secs_f64(1. / 60.); // 60 FPS
     App::new()
-        .add_plugins(RatatuiPlugins::default())
-        .add_plugins(ScheduleRunnerPlugin::run_loop(wait_duration))
-        .add_systems(PreUpdate, keyboard_input_system)
-        .add_systems(Update, hello_world.pipe(exit_on_error))
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: None,
+                close_when_requested: false,
+                exit_condition: ExitCondition::DontExit, // exit in controls.rs
+            }),
+            RatatuiPlugins::default(),
+            SimulatorPlugins,
+            TuiPlugin,
+        ))
         .run();
-}
-
-fn hello_world(mut context: ResMut<RatatuiContext>) -> color_eyre::Result<()> {
-    context.draw(|frame| {
-        let text = ratatui::text::Text::raw("hello world\nPress 'q' to Quit");
-        frame.render_widget(text, frame.area())
-    })?;
-    Ok(())
-}
-
-fn keyboard_input_system(mut events: EventReader<KeyEvent>, mut exit: EventWriter<AppExit>) {
-    use crossterm::event::KeyCode;
-    for event in events.read() {
-        match event.code {
-            KeyCode::Char('q') | KeyCode::Esc => {
-                exit.send_default();
-            }
-            _ => {}
-        }
-    }
 }
