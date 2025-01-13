46030a663e6cba24e06c4d88296e82ba09682854
Philip Linden <lindenphilipj@gmail.com>
2025-01-12 22:43:36 -0500

    experimental: custom primitives and raycasts

--- crates/yahs/src/geometry/primitives.rs
+++ crates/yahs/src/geometry/primitives.rs
@@ -9,39 +9,3 @@ use bevy::{
     prelude::*,
 };
 
-
-/// Enum listing the shapes that can be sampled
-#[derive(Clone, Copy)]
-enum Shape {
-    Cuboid,
-    Sphere,
-    Capsule,
-    Cylinder,
-    Tetrahedron,
-    Triangle,
-}
-struct ShapeMeshBuilder {
-    shape: Shape,
-}
-
-
-impl Meshable for Shape {
-    type Output = ShapeMeshBuilder;
-
-    fn mesh(&self) -> Self::Output {
-        ShapeMeshBuilder { shape: *self }
-    }
-}
-
-impl MeshBuilder for ShapeMeshBuilder {
-    fn build(&self) -> Mesh {
-        match self.shape {
-            Shape::Cuboid => CUBOID.mesh().into(),
-            Shape::Sphere => SPHERE.mesh().into(),
-            Shape::Capsule => CAPSULE_3D.mesh().into(),
-            Shape::Cylinder => CYLINDER.mesh().into(),
-            Shape::Tetrahedron => TETRAHEDRON.mesh().into(),
-            Shape::Triangle => TRIANGLE_3D.mesh().into(),
-        }
-    }
-}
